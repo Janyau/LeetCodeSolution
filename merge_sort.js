ff9aeee2383281966568000ddd3319b049cbe8b7
Janyau <janyaugoo@gmail.com>
2021-04-30 10:28:24 +0800

    merge sort

---
+++ merge_sort.js
@@ -0,0 +1,53 @@
+/**
+|        | 时间复杂度 | 空间复杂度 | 稳定排序 |
+|  归并   | O(nlogn) |   O(n)   |   是    |
+ */
+
+function mergeSort(list) {
+    const tempList = new Array(list.length)
+    sort(list, 0, list.length - 1, tempList)
+}
+
+function sort(list, left, right, tempList) {
+    // 注意条件, left == right 时会出现死循环, 需要 return
+    if (left >= right) return
+    const middle = parseInt((left + right) >> 1)
+    sort(list, left, middle, tempList)
+    sort(list, middle + 1, right, tempList)
+    merge(list, left, middle, right, tempList)
+}
+
+function merge(list, left, middle, right, tempList) {
+    let i = left
+    let j = middle + 1
+    let t = 0
+    while(i <= middle && j <= right) {
+        if (list[i] <= list[j]) {
+            tempList[t++] = list[i++]
+        } else {
+            tempList[t++] = list[j++]
+        }
+    }
+
+    // 判断哪个子数组中有剩余的数据, 并将剩余的数据拷贝到临时数组 tempList
+    while(i <= middle) {
+        tempList[t++] = list[i++]
+    }
+    while(j <= right) {
+        tempList[t++] = list[j++]
+    }
+
+    // 将 tempList 中的数组拷贝回 list
+    for (let k = 0; k < right - left + 1; k++) {
+        list[left + k] = tempList[k]
+    }
+    // console.log('----' ,tempList, list);
+}
+
+function randArray(len, min, max) {
+	return Array.from({length:len}, v=> Math.floor(Math.random()*(max-min)) + min)
+}
+
+let list = randArray(100, 1, 1000)
+mergeSort(list)
+console.log('--------' ,list);
